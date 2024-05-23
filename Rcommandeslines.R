93655ca903b5c153232e8a461237f2f4ef0aa142
Boudemagh mohamed lamine <64548423+Bibito21@users.noreply.github.com>
2024-05-23 17:49:33 +0100

    Delete Rcommandeslines.R

--- Rcommandeslines.R
+++
@@ -1,7 +0,0 @@
-# Les Packages
-
-install.packages("readxl") # 
-install.packages("ggpubr") #
-install.packages("gridExtra") # 
-install.packages("ggplot2") # représentation graphique
-install.packages("reshape") # transformer les données
