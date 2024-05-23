b875ac711f99b1d85437fba16f2d045ecc221527
Boudemagh mohamed lamine <bibito02@hotmail.com>
2024-05-23 17:02:29 +0100

    Create Rcommandeslines.R

---
+++ Rcommandeslines.R
@@ -0,0 +1,7 @@
+# Les Packages
+
+install.packages("readxl") # 
+install.packages("ggpubr") #
+install.packages("gridExtra") # 
+install.packages("ggplot2") # représentation graphique
+install.packages("reshape") # transformer les données
